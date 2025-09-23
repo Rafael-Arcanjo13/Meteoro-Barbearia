77f549ff3303cf200869b72e5f0855f13ece0808
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-09-22 23:14:38 -0300

    feat(all): animações adicionadas

--- src/js/app.js
+++ src/js/app.js
@@ -62,3 +62,20 @@ window.onload = () => {
     };
     setCurrentDot();
 }
+
+/* ------------------ ANIMATION SCROLL ------------------ */
+
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+});
+
+const elements = document.querySelectorAll('.hidden');
+
+
+elements.forEach( element => myObserver.observe(element) )
